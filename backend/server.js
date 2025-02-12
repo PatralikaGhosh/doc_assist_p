f59cb57d14ed4e1b300a047aa91b83af40bb5d9b
Patralika Ghosh <ghoshpatra2002@gmail.com>
2025-02-11 20:27:14 -0600

    yay

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,6 @@ app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-// Accessing the database
-// console.log(process.env.MONGO_URL);
-
 app.listen(5000, () => {    
     connectDB();
     console.log('Server is running on port 5000 : localhost:5000');
