0ef63062e22898040c84959c6f3892839edb1d5a
Patralika Ghosh <ghoshpatra2002@gmail.com>
2025-02-22 00:23:20 -0600

    made nlp better

--- backend/keyword_processor.py
+++ backend/keyword_processor.py
@@ -1,14 +1,32 @@
 import sys
 import json
 import nltk
+import re
+
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
+# nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('wordnet') 
+
 def extract_keywords(text):
-    words = word_tokenize(text.lower())
+    words = re.findall('\w+', text.lower())
+    # words = word_tokenize(text.lower())
     stop_words = set(stopwords.words("english"))
-    keywords = [word for word in words if word.isalnum() and word.lower() not in stop_words]
-    return keywords
+    keywords = [word for word in words if word.isalpha() and word.lower() not in stop_words]
+    # Remove hello, hi, hey, etc.
+    keywords = [word for word in keywords if word not in ["hello", "hi", "hey"]]
+
+    # Stemming
+    ss = nltk.SnowballStemmer(language = 'english')
+    keywords_stemmed = [ss.stem(word) for word in keywords]
+
+    return keywords_stemmed
+
+def clean_text(text):
+    return text.replace("\n", " ").replace("\r", " ").replace("\t", " ")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -16,6 +34,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     input_text = sys.argv[1]
-    keywords = extract_keywords(input_text)
+    cleaned_text = clean_text(input_text)
+    keywords = extract_keywords(cleaned_text)
 
     print(json.dumps(keywords))  # Ensure JSON output
